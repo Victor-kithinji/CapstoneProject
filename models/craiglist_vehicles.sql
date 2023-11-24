fe8f3bd1bb8ef535ead5d7584efafb5eb16d7627
victor kithinji <Kithinjivictor69@gmail.com>
2023-11-24 17:48:42 +0000

    Capstone Project

--- models/craiglist_vehicles.sql
+++ models/craiglist_vehicles.sql
@@ -1,23 +1,26 @@
-with craiglist_vehicles as(
-select 
-id,
-region,
-price,
-year,
-manufacturer,
-model,
-condition,
-cylinders,
-fuel,
-odometer,
-title_status,
-transmission,
-drive,
-size,
-type,
-paint_color,
-state,
-posting_date
-from capstoneproject.craiglistvehicles.craiglist_vehicles
+{{ config(
+    materialized = "table"
+) }} 
+WITH craiglist_vehicles AS (
+    SELECT
+        id,
+        region,
+        TRY_CAST(COALESCE(NULLIF(price, 'None'), '0') AS INTEGER) AS price,
+        year,
+        manufacturer,
+        model,
+        condition,
+        cylinders,
+        fuel,
+        TRY_CAST(COALESCE(NULLIF(odometer, 'None'), '0') AS INTEGER) AS odometer,
+        title_status,
+        transmission,
+        drive,
+        size,
+        type,
+        paint_color,
+        state,
+        TRY_CAST(posting_date AS DATETIME) AS posting_date
+    FROM capstoneproject.craiglistvehicles.craiglist_vehicles
 )
-select * from craiglist_vehicles
+SELECT * FROM craiglist_vehicles
